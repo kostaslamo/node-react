f1e91274b840170231b3ddefcc6828d609cab477
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2020-07-08 15:59:13 +0300

    Added simple frontend for react app

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './css/App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+const App = () => {
+  return <div className="App">Hello from React App!</div>;
+};
+
+export default App;
