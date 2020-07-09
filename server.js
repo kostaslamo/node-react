43187757e3dab1ab1d7956dce795879fd7fcde21
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2020-07-09 18:50:50 +0300

    Changes
    - Commented code serving react application from Express server 
    - Changed port listening for requests for react app in container
    - Added scripts in package.json of backend
    - Changed proxy form backend to localhost as it is used by react scripts development server to proxy the requests

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(
 app.use(bodyParser.json({ limit: '50mb' }));
 
 // Should be in the end of the file, after requiring all other routes so /api routes should be registered and not having problem making axios requests to API
-if (process.env.NODE_ENV === 'production') {
-  const root = path.join(__dirname, 'client', 'build');
-  app.use(express.static(root));
-  // Handles any requests that don't match the ones above serving react routes
-  app.get('*', (req, res) => {
-    res.sendFile('index.html', { root });
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const root = path.join(__dirname, 'client', 'build');
+//   app.use(express.static(root));
+//   // Handles any requests that don't match the ones above serving react routes
+//   app.get('*', (req, res) => {
+//     res.sendFile('index.html', { root });
+//   });
+// }
 
 app.listen(port, () => global.logger.info(`Server up and running in ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} mode, on port ${port} !`));
